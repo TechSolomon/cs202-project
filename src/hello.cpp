a33ba366ea8de33c4deed8be8d3a93b9862d7031
Benjamin Stream <brstream@alaska.edu>
2021-03-30 11:25:59 -0800

    SFML Libraries added

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML\Graphics.hpp>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -11,6 +12,23 @@ int main() {
     cout << "Adrian has access and can contribute" << endl;
     cout << "Press ENTER to quit... ";
     while (cin.get() != '\n');
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
