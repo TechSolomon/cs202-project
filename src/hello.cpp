2b37cafcb4d1c95c928902770236f2c6c6e3f291
Jay-Mark <jpascua3@alaska.edu>
2021-03-30 10:37:57 -0800

    Jay-Mark test push

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,6 +6,8 @@ using std::endl;
 int main(){
     cout << "Hello, world." << endl;
 
+    cout << "Jay-Mark has access and can contribute" << endl;
+
     cout << "Press ENTER to quit... ";
     while (cin.get() != '\n');
 
