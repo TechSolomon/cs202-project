562f6856cda0c99d1e2dcc9bf1cb41f2fa7692e0
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-04-13 09:10:46 -0800

    add: ranking system (initial files)

---
+++ src/ranking.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Solomon on 4/8/21.
+//
+
+#include "ranking.h"
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, card ranking." << endl;
+}
---
+++ src/ranking.h
@@ -0,0 +1,8 @@
+//
+// Created by Solomon on 4/8/21.
+//
+
+#ifndef CS202_PROJECT_RANKING_H
+#define CS202_PROJECT_RANKING_H
+
+#endif //CS202_PROJECT_RANKING_H
