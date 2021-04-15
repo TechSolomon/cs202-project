dd7af40101cacb0a4ff76a385752f8f49ec6d7ed
Akantonio <adrianvantonio@yahoo.com>
2021-04-14 17:41:43 -0800

    shuffle function

--- src/deck.cpp
+++ src/deck.cpp
@@ -41,6 +41,13 @@ void Deck::resetCard()
 	
 }
 
+void Deck::shuffle()
+{
+	std::random_device rd;
+	std::default_random_engine rng(rd());
+	std::shuffle(begin(playableCards), end(playableCards), rng);
+}
+
 void Deck::drawCards(std::vector<int, std::string>& v,int amount)
 {
 }
