4bf97e0668d16f6c494c71520eabf30f0131d7db
Jay-Mark <jpascua3@alaska.edu>
2021-04-20 17:39:23 -0800

    Game class: Added everyoneCalled function to indicate if round can advance, added vector<pair, string> river, added int _roundPhase

--- src/game.cpp
+++ src/game.cpp
@@ -34,13 +34,17 @@ void Game::setup(int& players) { // Constructs deck and player objects before st
 
 void Game::gameLoop(Game& game) { // Starts a round of poker
 	// 1. Go through each players' turn (each player can check, bet, call, etc.)
+	if (_roundPhase == 0) { // Betting phase
+		p1.getPlayerInput(game, game.p1);
+		p2.getPlayerInput(game, game.p2);
+		p3.getPlayerInput(game, game.p3);
+		p4.getPlayerInput(game, game.p4);
+	}
+	
 	// 2. Deal a card to the river
 	// 3. If there are 5 cards in the river, check which player has the highest ranking hand
 	// 4. Round ends, winning player gets the pot, loop back to beginning
-	p1.getPlayerInput(game, game.p1);
-	p2.getPlayerInput(game, game.p2);
-	p3.getPlayerInput(game, game.p3);
-	p4.getPlayerInput(game, game.p4);
+	
 
 }
 
@@ -58,4 +62,23 @@ void Game::setCurrentBet(const int& bet) {
 
 int Game::getCurrentBet() const {
 	return _currentBet;
+}
+
+bool Game::everyoneCalled() { // Advances round phase if everyone has called
+
+	std::vector<Player> playersStillPlaying;
+	if (p1._isFolded == false)
+		playersStillPlaying.push_back(p1);
+	if (p2._isFolded == false)
+		playersStillPlaying.push_back(p2);
+	if (p3._isFolded == false)
+		playersStillPlaying.push_back(p3);
+	if (p4._isFolded == false)
+		playersStillPlaying.push_back(p4);
+
+	for (const auto& x : playersStillPlaying) {
+		if (x.playerCurrentBet != _currentBet)
+			return false;
+	}
+	return true;
 }
--- src/game.h
+++ src/game.h
@@ -25,13 +25,18 @@ public:
 	void setCurrentBet(const int& bet);
 	int getCurrentBet() const;
 
+	bool everyoneCalled(); // Advances round phase if everyone has called
+
 	friend void Player::raise(Game& game, const int& raise);
 
 private:
 	Deck _cards;
+	std::vector<std::pair<int, std::string>> _river;
 	int _numPlayers;
 	int _pot = 0; // Money earned after winning round
 	int _currentBet = 0; // The round's current highest bet
+	int _roundPhase = 0; // What phase the round is currently on (Phase 0: Betting, Phase 1: Deal three cards to river
+						 // Phase 2: Deal one card to river, Phase 3: Determine winner) PHASES ALTERNATE BETWEEN BETTING AND DEALING CARDS TO RIVER
 };
 
 
