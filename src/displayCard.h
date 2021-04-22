5277a115aa0392ec96fc68ea37537442cb5d4adf
Akantonio <adrianvantonio@yahoo.com>
2021-04-22 00:30:56 -0800

    moved some functions

--- src/displayCard.h
+++ src/displayCard.h
@@ -16,14 +16,13 @@
 #include <vector>
 
 
-
+void cardDisplayValue(std::vector<displayCard>& putCards, std::vector<std::pair<int, std::string>>& cards);//Do this first
+void offsetPosition(std::vector<displayCard>& cardPool, float y);//Moves cards to the right by 50*wherever it is in the vector
+void initialPosition(std::vector<displayCard>& cardPool, float x, float y);
+void screenCards(std::vector<displayCard>& cardPool, sf::RenderWindow& display);//Draws on a RenderedWindow
 
 static sf::Texture cardMap;
 class displayCard {
-    friend void cardDisplayValue(std::vector<displayCard>& putCards, std::vector<std::pair<int, std::string>>& cards);//Do this first
-    friend void offsetPosition(std::vector<displayCard>& cardPool,float y);//Moves cards to the right by 50*wherever it is in the vector
-    friend void initialPosition(std::vector<displayCard>& cardPool, float x, float y);
-    friend void screenCards(std::vector<displayCard>& cardPool,sf::RenderWindow &display);//Draws on a RenderedWindow
 public:
     displayCard(int LorR, int UorB);
     sf::Sprite cardSprite;
